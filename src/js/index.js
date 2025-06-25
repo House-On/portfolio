68134f884f51703032fac5fe99ca6f2895474beb
Marcos Henrique <marcosfhtrabalho@gmail.com>
2025-06-25 12:05:03 -0300

    test: adição do link do pages para o Readme

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,24 @@
 
-let botao = document.getElementById("btnTeste");
+let select = document.getElementById('option')
 
-botao.addEventListener('click', () => {
-    window.location.href = "./src/pages/teste.html"
+select.addEventListener('change', (event) => {
+    const valor = event.target.value;
+
+    console.log(valor);
+
+    if(valor) {
+        switch(valor){
+            case 'Github':
+                window.location.href = "./src/pages/teste.html"
+                break;
+            case 'Ideia':
+                console.log('Indo para ideia')
+                break;
+            case 'Telas':
+                console.log('Indo para as telas')
+                break;
+            default:
+                console.log('Nada')
+        }
+    }
 })
